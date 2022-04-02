4ad894e32e6464accebe6591a11ba07d1616e450
Lawrence <lxren460@gmail.com>
2022-04-01 19:13:38 -0700

    set up react files

---
+++ letmeinubc-react/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Notificationbox from "./components/Notificationbox";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Notificationbox />
+    </div>
+  );
+}
+
+export default App;
---
+++ letmeinubc-react/src/components/Notificationbox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Notificationbox.css'
+
+function Notificationbox() {
+  return (
+    <div id='Notification-div'>Notificationbox</div>
+  )
+}
+
+export default Notificationbox
