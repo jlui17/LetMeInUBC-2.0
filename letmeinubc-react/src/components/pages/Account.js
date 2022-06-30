0a812b132e9ffb30e7ae66e9b1a7923088e5e6ac
Lawrence Tang <lawrence@lawrences-mbp.lan>
2022-06-29 20:26:33 -0700

    added pages

---
+++ letmeinubc-react/src/components/pages/Account.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SideBar from "../SideBar";
+
+export default function Account() {
+  return (
+    <>
+      <div>
+        re
+        <SideBar />
+      </div>
+    </>
+  );
+}
