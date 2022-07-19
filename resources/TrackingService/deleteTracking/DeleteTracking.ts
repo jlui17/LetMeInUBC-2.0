0ad142582ede12d0039af6124bc7e1cb92dd3cb0
jlui17 <justinlui17@gmail.com>
2022-07-18 23:01:46 -0700

    Update delete tracking to use v3 sdk

--- resources/TrackingService/deleteTracking/DeleteTracking.ts
+++ resources/TrackingService/deleteTracking/DeleteTracking.ts
@@ -1,4 +1,4 @@
-import { DynamoDB } from "aws-sdk";
+import { DynamoDBClient, DeleteItemCommand } from "@aws-sdk/client-dynamodb";
 
 const TRACKING_TABLE_NAME = process.env.TRACKING_TABLE_NAME || "";
 
@@ -13,26 +13,20 @@ interface courseTrackingParams {
 export const deleteTracking = async (
   event: courseTrackingParams
 ): Promise<boolean> => {
-  const db = new DynamoDB.DocumentClient();
+  const ddbClient = new DynamoDBClient({});
   const { department, section, number, session, email } = event;
   const courseName = `${session} ${department} ${number} ${section}`;
 
+  const deleteRequest = new DeleteItemCommand({
+    TableName: TRACKING_TABLE_NAME,
+    Key: { courseName: { S: courseName }, email: { S: email } },
+  });
+
   let response: boolean = true; // using this comment to rebuild lambda
 
-  await db
-    .delete({
-      TableName: TRACKING_TABLE_NAME,
-      Key: {
-        courseName: courseName,
-        email: email,
-      },
-    })
-    .promise()
-    .catch((error) => {
-      response = false;
-    });
+  await ddbClient.send(deleteRequest).catch((error) => {
+    response = false;
+  });
 
   return response;
 };
-
-exports.handler = deleteTracking;
