3b5031d285a78531b22703aac7a90c5fa200777e
jlui17 <justinlui17@gmail.com>
2022-08-06 17:57:48 -0700

    Widen info widget

--- letmeinubc-react/src/components/InfoWidget.js
+++ letmeinubc-react/src/components/InfoWidget.js
@@ -1,6 +1,6 @@
 const InfoWidget = (props) => {
   return (
-    <div className="bg-cool-blue text-white rounded-lg flex items-center flex-col shadow-lg gap-2 w-44">
+    <div className="bg-cool-blue text-white rounded-lg flex items-center flex-col shadow-lg gap-2 w-48">
       <h2 className="text-xl p-6 pb-0">{props.title}</h2>
       <div className="flex justify-center items-center">{props.icon}</div>
     </div>
